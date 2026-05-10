abbd003b851f1d5d6552a5a06d87bd38bf7d4a72
Ilya Sennikov <rodina89657774015@gmail.com>
2026-05-10 17:36:55 +0300

    [MTR] Запуск корутины

--- src/excempl.py
+++ src/excempl.py
@@ -1,10 +1,18 @@
 import asyncio
+from time import time
 
 
-async def f():
-    await asyncio.sleep(1)
-    print("end")
+async def f(time_sl: int) -> None:
+    """Пробная корутина"""
+    time_sleep = time()
+    await asyncio.sleep(time_sl)
+    print(f" Время выполенния : {time() - time_sleep:.2f}")
 
 
-def main():
-    asyncio.run(f())
+async def main() -> None:
+    """Точка входа асинх. приложения"""
+    for time_sl in range(1, 10):
+        await f(time_sl)
+
+
+asyncio.run(main())
